17d38700e0448bf3a98eaab26c7d36fea83c315b
Pushpundu Jana <91383539+pushpendu00@users.noreply.github.com>
2023-09-04 13:59:34 +0530

    Add files via upload

---
+++ config/mongoose.js
@@ -0,0 +1,24 @@
+/** ------------------ IMPORTING PACKAGE ------------------ **/
+const mongoose = require("mongoose");
+
+
+/** ------------------ MAKING CONNECTION ------------------ **/
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/CSV").then(()=>{
+    console.log('Connection successful!');
+}).catch((err) => console.log("no connection " + err));
+
+//setting it to db
+const db = mongoose.connection;
+
+/** ------------------ CHECKING CONNECTION ------------------ **/
+//if error occurs
+db.on("error", console.error.bind(console, "Error connecting to DB"));
+// when db connects successfully
+db.once("open", function(){
+    console.log("Successfully connected to DB");
+});
+
+/** ------------------ EXPORTING DB ------------------ **/
+module.exports = db;
